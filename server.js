31c98ef56a0e276371c90cbe26d8706b001e3e33
Rajdeep Jash <rajdeepjash2070@gmai.com>
2022-06-24 10:52:00 +0530

    make it better

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ mongoose.connect(DB,{
 const PORT=process.env.PORT || 8000;
 
 
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static("frontend/build"));
+    const path=require("path");
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'frontend/build/index.html'));
+    })
+}
+
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
