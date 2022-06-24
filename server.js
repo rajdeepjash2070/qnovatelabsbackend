26edcde5990f90154d3e6465c6d3dda1f77f80ee
Rajdeep Jash <rajdeepjash2070@gmai.com>
2022-06-24 15:32:25 +0530

    make it better

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV=="production"){
     app.use(express.static("frontend/build"));
     const path=require("path");
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend/build/index.html'));
+        res.sendFile(path.resolve(__dirname,'qnovatefrontend/build/index.html'));
     })
 }
 
